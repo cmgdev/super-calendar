ea281492f85303d257c1f57297b0dfb4d994f3d6
cmgdev <christina.gay@gmail.com>
2014-10-03 15:54:52 -0500

    parsing the received json and converting to web format

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var http = require('http');
+var cheerio = require('cheerio');
+var moment = require('moment');
 
 var app = express();
 
@@ -10,10 +12,10 @@ var server = app.listen(3000, function(){
 app.get('/weather', function(req, res){
 	lat = req.param('lat', '35');
 	lon = req.param('lon', '139');
+	callback = req.param('callback');
 
 	path = 'http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + lat + '&lon=' + lon + '&cnt=10&mode=json';
 	console.log('Using path ' + path);
-
 
 	http.get(path, function(getRes){
 		console.log("Got response: " + getRes.statusCode);
@@ -22,12 +24,26 @@ app.get('/weather', function(req, res){
 			data += chunk;
 		});
 		getRes.on('end', function() {
-			toReturn = data;
-			res.send(toReturn);
+			recvJson = JSON.parse(data).list;
+			sendJson = [];
+
+			$ = cheerio.load(recvJson);
+			$(recvJson).each(function(){
+				sendJson.push({
+					title: this.temp.max + '/' + this.temp.min,
+					start: moment(this.dt, 'X')
+				});
+			});			
+
+
+			sendTxt = callback + '(\'' + JSON.stringify(sendJson) + '\');';
+			console.log(sendTxt);
+			res.set('Content-Type', 'application/javascript');	
+			res.send(sendTxt);
 		})
 	    }).on('error', function(e) {
 		errorMsg = "Got error: " + e.message;
 		console.log(errorMsg);
 		res.send(errorMsg);
-	    });
+	});
 });
